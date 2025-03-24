6bb9d543548e715433091895359e07faf2c180e2
ankit-yadav90 <ankityadav73699@gmail.com>
2025-03-24 11:35:21 +0530

    chenge sting value

--- first.js
+++ first.js
@@ -1,6 +1,11 @@
 //for of loop
 
-let str = "AppnaCollege"
-for(let i of str) {
-    console.log("i =",i);
+let str = "JavaScript";
+
+let size = 0;
+for(let val of str) {
+    console.log("val =",val);
+    size++;
 }
+
+console.log("string size = ", size);
